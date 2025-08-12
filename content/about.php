bd10cb2b9c315d79acd06b9e927e2fecd59d36f7
Blukitty07 <159348363+Blukitty07@users.noreply.github.com>
2025-08-12 13:23:06 +1200

    Create about.php

---
+++ content/about.php
@@ -0,0 +1,108 @@
+<h1>About</h1>
+<!-- information general -->
+<h3>What is this database for?</h3>
+<p>This database aims to be a small apprication place in which people can discover vocaloid, utauloid and synthV songs and be able to easily discover information about these said songs</p>
+<h3>What are Vocaloids?</h3>
+<p>Vocaloids are a voice synthesizer computer softwares that are developed by Yamaha, and are used as instruments in songs. The vocaloid software uses voicebanks to generate vocal sounds (which is done by hand with absolutely no AI involved). The vocaloid voicebank are created by having a voice provider record a bank of voice samples, which is what the engine uses to construct songs. To be able to use vocaloids you must purchase the software (like you would any other instrument) to be able to use it. The first vocaloids were made in spain in 2000, but it wasn’t until Hatsune Miku was created in 2007 by YAMAHA that the popularity of vocaloids surged.</p>
+<h3>What is the difference between Vocaloids, Utauloids and SynthV?</h3>
+<p>When people talk about vocaloids, often the topic of utauloids and synthv come up. UTAU is a japanese program made in 2008, with the program itself and most voicebanks made by it being completely free. You can also make your own voicebanks in UTAU, and they are able to sing in basically any language, while delivering similar results to a vocaloid.</p><p> SynthV much like vocaloid will use create soundbanks using a voice provider to build the soundbank but they also add in AI in order to give it a more “lifelike” sound. They are also cheaper to purchase than vocaloids and have some free versions of SynthV soundbanks.</p>
+
+<!-- Images and individual information carasoul -->
+
+<h2> ✭ The most well-known characters ✭ </h2>
+
+
+<!-- Slideshow container -->
+<div class="slideshow-container">
+
+	<!-- Full-width images with number and caption text -->
+	<div class="first mySlides fade">
+		<div class="numbertext">1 / 8</div>
+		<div class="grid" <?php echo'body style="background-color:#b9fff3"'?>>
+			<img class="picture" alt="miku" src="images/miku.png">
+			<div class="info">
+				This is Hatsune Miku. <br>A vocaloid first released by Crypton Future Media on the 31st of August, 2007. Her voice was provided by Fujita Saki, and her image was created by Kei Garō. she is often associated with leeks because of a popular meme that orignated from a video of her spinning one and it has since stuck with her character. She is also associated with the number 39 because of a japanese pun in which it can be read as 'mi' 'ku'. She has the number 01 on her arm signifying how she was the first character in Crypton Future Media's 'Charcater Vocal Series'. Her most well-known song is 'World is Mine' by Supercell, relased in 2009.</div>
+		</div>
+	</div>
+
+	<div class="mySlides fade">
+		<div class="numbertext">2 / 8</div>
+		<div class="grid" <?php echo'body style="background-color:#ffefa8"'?>>
+			<img class="picture" alt="rin" src="images/rin.png">
+			<div class="info">
+				This is Kagamine Rin. <br>A vocaloid first released by Crypton Future Media on the 27th of December, 2007. Her voice is provided by Asami Shimoda. She is often associated with a mikan orange as her yellow her and white bow ressemble the mikan orange. She also has the number 02 on her arm as she is the second Vocaloid character in the Character Voice series, she shares this number with Kagamine Len as they were released as a pair. Her most well-known song is 'Young Girl A' by siinamota released in 2013. </div>
+		</div>
+	</div>
+
+	<div class="mySlides fade">
+		<div class="numbertext">3 / 8</div>
+		<div class="grid" <?php echo'body style="background-color:#ffe5a8"'?>>
+			<img class="picture" alt="len" src="images/len.png">
+			<div class="info">
+			This is Kagamine Len. <br> A vocaloid first released by Crypton Future Media on the 27th of December, 2007. His voice is provided by Asami Shimoda. He is often associated with bananas because his hair ressembles a bunch of bananas. He also has the number 02 on his arm as he is the second Vocaloid character in the Character Voice series, he shares this number with Kagamine Rin as they were released as a pair. One of his most well-known song is 'It's A Whole World' by Sooichi released in 2013. </div>
+		</div>
+	</div>
+	
+	<div class="mySlides fade">
+		<div class="numbertext">4 / 8</div>
+		<div class="grid" <?php echo'body style="background-color:#fde0ff"'?>>
+			<img class="picture" alt="luka" src="images/luka.png">
+			<div class="info">
+			This is Megurine Luka. <br> A vocaloid first released by Crypton Future Media on the 30th of January, 2009. Her voice is provided by Yū Asakawa. She is often associated with an octopus because of the Tako Luka cover of Popipo, in which Tako Luka (a Luka with octopus hair) dances to Popipo, she is also associated with frozen tuna, not only because it is a popular japanese dish but also as her name Megurine sounds similar to the japanese word for tuna which is maguro. She has 03 on her arm as she is the third character in the Character Voice series by Crypton Future Media Her most well-known songs is 'Double Lariat' by Megurine Luka released in 2009.</div>
+		</div>
+	</div>
+	
+	<div class="mySlides fade">
+		<div class="numbertext">5 / 8</div>
+		<div class="grid" <?php echo'body style="background-color:#ccfcbd"'?>>
+			<img class="picture" alt="gumi" src="images/gumi.png">
+			<div class="info">
+			This is GUMI. <br> A vocaloid first released by Internet Co., Ltd in June, 2009. Her voice is provided by Megumi Nakajima. She is often associated with a carrot based on her outfit. Her most well-known song is ECHO by Crusher-P, released in 2014. She is my person favourite vocaloid. Her most popular songs such as 'Copycat', 'ECHO', and 'Housewife radio', 'Monster', and more have become incredibly popular animation memes in the online art space. An animation meme is a a genre of internet animation where videos have distinctive, and often simple structures which are recreated by numerous people and/or are specifically designed to be copied (fan video wiki). While the other vocaloids have also contributed to the animation meme space with their songs, GUMI has by far the most recognisable ones to her name.</div>
+		</div>
+	</div>
+	
+	<div class="mySlides fade">
+		<div class="numbertext">6 / 8</div>
+		<div class="grid" <?php echo'body style="background-color:#404040"'?>>
+			<img class="picture" alt="meiko" src="images/meiko.png">
+			<div class="info" <?php echo'body style="color:#fff"'?>>
+			This is MEIKO. <br> A vocaloid first released by Crypton Future Media on the 5th of November, 2004. her voice is provided by Meiko Haigo. She is often associated with Japanese Sake in paticular the Ozeki One Cup because of a song made by One Cup-P, in which he complains about his delayed Hatsune Miku order and says he will drown his sorrows in achohol. He has Meiko sing this song and the association has stuck, especially because of her more mature appearance. Her most well-known song is 'Nostalogic' by yuukiss released in 2008.</div>
+		</div>
+	</div>
+	
+	<div class="mySlides fade">
+		<div class="numbertext">7 / 8</div>
+		<div class="grid" <?php echo'body style="background-color:#94bbff"'?>>
+			<img class="picture" alt="kaito" src="images/kaito.png">
+			<div class="info">
+			This is Kaito. <br> A vocaloid first released by Crypton Future Media on the 14th February, 2006. His voice is provided by Naoto Fūga. His is often associated with light blue ice cream bar because of his blue and white colour scheme and a joke video which has since stuck to his character. His most well-known song is 'SHANTI' by wotaku released in 2021.</div>
+		</div>
+	</div>
+	
+	<div class="mySlides fade">
+		<div class="numbertext">8 / 8</div>
+		<div class="grid" <?php echo'body style="background-color:#ff9191"'?>>
+			<img class="picture" alt="teto" src="images/teto.png">
+			<div class="info">
+			This is Kasane Teto. <br> A  fan Utau/SynthVloid first created by users of the Japanese imageboard 2channel as an April Fools prank as a troll vocaloid. But since she has been made into an Utauloid and later a SynthV. Her voice is provided by Mayo Oyamano. She is often associated with baguettes because that's part of how she was designed as a joke by fans. Other details that were included in her joke release is that she is 31 years old, but is a Chimera so she is actualy only 15 and a half years old. Her most well-known song is 'Override' by Yoshida Yasei released in 2023.</div>
+		</div>
+	</div>
+	
+
+	<!-- Next and previous buttons -->
+	<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+	<a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+	<span class="dot" onclick="currentSlide(1)"></span>
+	<span class="dot" onclick="currentSlide(2)"></span>
+	<span class="dot" onclick="currentSlide(3)"></span>
+	<span class="dot" onclick="currentSlide(4)"></span>
+	<span class="dot" onclick="currentSlide(5)"></span>
+	<span class="dot" onclick="currentSlide(6)"></span>
+	<span class="dot" onclick="currentSlide(7)"></span>
+	<span class="dot" onclick="currentSlide(8)"></span>
+</div>
